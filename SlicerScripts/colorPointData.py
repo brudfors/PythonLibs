b79a79bab7a2232b22e0168d4fd56eecb9350aac
Mikael Brudfors <brudfors@hggm.es>
2015-09-14 17:24:53 +0200

    ADD: Script to color a vtkPolyData.

---
+++ SlicerScripts/colorPointData.py
@@ -0,0 +1,26 @@
+def colorPointData(modelNode, min, max, newMin, newMax):
+  polyData = modelNode.GetPolyData()
+  # Create rainbow color table  
+  colorTable=slicer.vtkMRMLColorTableNode()
+  colorTable.SetTypeToRainbow ()  
+  # Calculate normalizing constants
+  n = (newMax-newMin)/(max-min)
+  # Create color array
+  colorArray = vtk.vtkDoubleArray()
+  colorArray.SetNumberOfComponents(4)
+  colorArray.SetName('Colors')
+  polyData.GetPointData().SetScalars(colorArray)    
+  # Normalize and add color scalar
+  color = [0, 0, 0, 0]
+  for i in range(polyData.GetNumberOfPoints()):
+    ras = polyData.GetPoint(i)
+    tableValue = 1 + (ras[1] - min) * n
+    colorTable.GetColor(int(tableValue), color) # 0 -> 255
+    colorArray.InsertTuple(i, color)
+    
+p=getNode('Points_2015-09-11_16-28-47')
+colorPointData(p, -30, 0, 0, 255)
+
+m=p.GetModelDisplayNode()
+m.SetActiveScalarName('Colors')
+m.SetScalarVisibility(True)
