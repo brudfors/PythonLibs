ecc5e37875dc41a848816e1ed87f5e70a31572d8
Mikael Brudfors <brudfors@hggm.es>
2015-09-16 13:23:44 +0200

    ENH: Minor modification.

--- SlicerScripts/filterConoProbePoints.py
+++ SlicerScripts/filterConoProbePoints.py
@@ -3,19 +3,28 @@ def filterConoProbePoints(modelNode, csvPath, snrThreshold, distanceMinimumValue
   snr = []
   distance = []
   ras = []
+  count = 0
   with open(csvPath, 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
+      if count == 0:      
+        count += 1
+        continue      
       ras.append(row[0:3])
-      snr.append(row[4])
-      distance.append(row[3])
+      snr.append(float(row[4]))
+      distance.append(float(row[3]))
+      if count == 1:
+        print ras[0]
+        print snr[0]
+        print distance[0]
+        count += 1      
   # Create new vtkPolyData
   newPoints = vtk.vtkPoints()
   newVertices = vtk.vtkCellArray()               
   newPolyData = vtk.vtkPolyData()
   newPolyData.SetPoints(newPoints)
   newPolyData.SetVerts(newVertices)
-  # Filter accordingly to the input parameters  
+  # Filter accordingly to the input parameters 
   for idx in range(len(distance)):    
     if (snr[idx] > snrThreshold and distance[idx] < distanceMaximumValue and distance[idx] > distanceMinimumValue):        
       addPointToPolyData(newPolyData, ras[idx])
@@ -26,7 +35,7 @@ def filterConoProbePoints(modelNode, csvPath, snrThreshold, distanceMinimumValue
 def addPointToPolyData(polyData, ras):      
   pid = vtk.vtkIdList()
   pid.SetNumberOfIds(1);
-  temp = polyData.GetPoints().InsertNextPoint(ras[0], ras[1], ras[2])    
+  temp = polyData.GetPoints().InsertNextPoint(float(ras[0]), float(ras[1]), float(ras[2]))    
   pid.SetId(0, temp)    
   polyData.GetVerts().InsertNextCell(pid)        
   polyData.Modified() 
@@ -47,4 +56,4 @@ def createModelNode(name, color):
   
 createModelNode('FilteredPoints', [1,0,0])
 m = getNode('FilteredPoints')
-filterConoProbePoints(m, 'H:/src/conoprobe/SlicerModules/ConoProbeConnector/Output/2015-09-14/Points_2015-09-14_19-15-57.csv', 93, 100, 400)
+filterConoProbePoints(m, 'H:/ConoProbe_Experiments_2015-09-11/L/Points_2015-09-11_16-28-47.csv', 91, 150, 350)
